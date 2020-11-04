809311d5c548f36cebe32c4628e5d20cd486b823
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-11-03 16:45:15 -0800

    Repair tests ready

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,4 +1,23 @@
 const enhancer = require('./enhancer.js');
 describe("Enhancer unit tests", () => {
-    
+    it("repairs", () => {
+        expect(enhancer.repair({
+            name: "Bowl",
+            enhancement: 20,
+            durability: 7
+        })).toEqual({
+            name: "Bowl",
+            enhancement: 20,
+            durability: 100
+        })
+        expect(enhancer.repair({
+            name: "Bowl",
+            enhancement: 20,
+            durability: '0'
+        })).toEqual({
+            name: "Bowl",
+            enhancement: 20,
+            durability: 100
+        })
+    })
 })
